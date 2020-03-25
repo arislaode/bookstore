a664b0578690200283713cc2faeb2703853012ad
La Ode Aris Saputra <laodearissaputra94@gmail.com>
2020-03-25 23:16:51 +0700

    environment in docker ,  update SECRET_KEY bookstore_project/settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dmav0_=o-ydriiyiehv9u+&4xm%n8xy)0n-9%fl9mw@l)v($%0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
