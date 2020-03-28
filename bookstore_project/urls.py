eff9d9ed5be64ed60b2885602415b9ae339ef768
La Ode Aris Saputra <laodearissaputra94@gmail.com>
2020-03-28 20:43:17 +0700

    performace bookstore app

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
 
@@ -28,3 +29,9 @@ urlpatterns = [
     path('books/', include('books.urls')),
     path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
